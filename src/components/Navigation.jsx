df6fc24fbc4c5e35231454271648738734ff3417
ophir0 <147547243+ophir0@users.noreply.github.com>
2024-03-07 18:46:04 +0000

    Set up Vite, npm, and create base structure

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,18 @@
+const Navigation = () => {
+    return (
+      <nav>
+        <div className="logo">
+          <h1>Eliséo Marrone</h1>
+          <h5>photographie</h5>
+        </div>
+        <ul>
+          <li href="#" className="AccueilTexte">Accueil</li>
+          <li href="#" className="SeriesTexte">Series</li>
+          <li href="#" className="InfosTexte">Infos</li>
+        </ul>
+      </nav>
+    );
+  };
+  
+  export default Navigation;
+  
