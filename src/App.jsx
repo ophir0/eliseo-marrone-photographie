61b2bd1d5c8c07e341f0225addc205819e120413
ophir0 <147547243+ophir0@users.noreply.github.com>
2024-03-07 18:55:34 +0000

    Installs yet-another-react-lightbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import Navigation from "./components/Navigation";
+import 
+  import Gallery from "./components/Gallery";
 
 const App = () => {
   
   return (
-  <Navigation />   
+  <Navigation />  
+  <Gallery /> 
   )
 }
 
