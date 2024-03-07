c4e8df1d8c7b74bea28ef3cd32f7b55ce4e84117
ophir0 <147547243+ophir0@users.noreply.github.com>
2024-03-07 19:12:29 +0000

    Revert "Installs yet-another-react-lightbox"
    This reverts commit 61b2bd1d5c8c07e341f0225addc205819e120413.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import "./App.css";
 import Navigation from "./components/Navigation";
-import 
-  import Gallery from "./components/Gallery";
 
 const App = () => {
   
   return (
-  <Navigation />  
-  <Gallery /> 
+  <Navigation />   
   )
 }
 
