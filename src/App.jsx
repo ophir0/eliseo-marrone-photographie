a28aabbc58f05d813cb9402fce4cace5bca53c97
ophir0 <147547243+ophir0@users.noreply.github.com>
2024-03-08 10:53:35 +0000

    Applies CSS to App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ import Navigation from "./components/Navigation";
 const App = () => {
   
   return (
-  <>
+  <div className="App">
     <Navigation />   
     <Accueil />    
-  </>
+  </div>
   )
 }
 
