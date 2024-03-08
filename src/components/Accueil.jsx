890d2022a5cf6af81e4b9ae58352f630a919cc2c
ophir0 <147547243+ophir0@users.noreply.github.com>
2024-03-08 11:21:17 +0000

    Wraps BrowserRouter to <App> component
    Uses the HTML5 History API, for modern browsers (2015 onwards)

--- src/components/Accueil.jsx
+++ src/components/Accueil.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Accueil = () => {
   return (
     <main className="acceuil-container">
-        <img className="accueil-image" src="/public/series/Thumbnails/Une nuit à Osaka.jpg"></img>
+        <img className="accueil-image" src="/series/Thumbnails/Une nuit à Osaka.jpg"></img>
     </main>
   )
 }
