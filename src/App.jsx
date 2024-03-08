d00f378374e49a726c4fb8a1f94ebe29e2e7a5c0
ophir0 <147547243+ophir0@users.noreply.github.com>
2024-03-08 11:41:28 +0000

    Implements render() in App.jsx and <Link> in Navigation.jsx  menu items

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
+import { Link } from 'react-router-dom';
 import "./App.css";
 import Accueil from "./components/Accueil";
 import Navigation from "./components/Navigation";
 
 const App = () => {
-  
-  return (
-  <div className="App">
-    <Navigation />   
-    <Accueil />    
-  </div>
-  )
-}
-
+  render(); {
+    return (
+      <div className="App">
+        <Navigation />;   
+        <Accueil />;
+      </div>
+      )
+  };    
+};
 export default App;
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,18 +1,19 @@
 const Navigation = () => {
-    return (
-      <nav>
-        <div className="logo">
-          <h1>Eliséo Marrone</h1>
-          <h5>photographie</h5>
-        </div>
-        <ul>
-          <li href="#" className="AccueilTexte">Accueil</li>
-          <li href="#" className="SeriesTexte">Series</li>
-          <li href="#" className="InfosTexte">Infos</li>
-        </ul>
-      </nav>
+     return (
+        <nav>
+          <div className="logo">
+            <h1>Eliséo Marrone</h1>
+            <h5>photographie</h5>
+          </div>
+          <ul>
+            <li><Link to="/">Accueil</Link></li>
+            <li><Link to="/Series">Series</Link></li>
+            <li><Link to="/Infos">Infos</Link></li>
+          </ul>
+        </nav>
     );
-  };
+  
+};
   
   export default Navigation;
   
