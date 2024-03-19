f0fe9efc96b4590c46dfbe4dd1431bf0f3cb3f22
ophir0 <147547243+ophir0@users.noreply.github.com>
2024-03-19 10:51:14 +0000

    Installs react-spring and animates accordions

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,4 +1,5 @@
 import { React, useState } from 'react'
+import { useSpring, animated } from "react-spring";
 
 function Accordion(props) {
     const [open, setOpen] = useState(false);
@@ -7,14 +8,42 @@ function Accordion(props) {
     //switch state
     setOpen(!open);
   };
+
+   //conditional styling
+   const styles = {
+    //if open is true, show content
+    accordionTitle: {
+      color: open ? "#10d6f5" : "#fff"
+    }
+  };
+  //open animation with react spring
+
+  const openAnimation = useSpring({
+    from: { opacity: "0", maxHeight: "25px" },
+    to: { opacity: "1", maxHeight: open ? "120px" : "25px" },
+    config: { duration: "300" }
+  });
+
+  //rotate animation
+  const iconAnimation = useSpring({
+    from: {
+      transform: "rotate(0deg)",
+      color: "#ffff"
+    },
+    to: {
+      transform: open ? "rotate(180deg)" : "rotate(0deg)",
+      color: open ? "#10d6f5" : "#fff"
+    },
+    config: { duration: "120" }
+  });
   return (
-    <div className="accordion__item">
-      <div className="accordion__header" onClick={toggleHandler}>
-        <h4>{props.title}</h4>
+    <animated.div className="accordion__item" style={openAnimation}>
+    <div className="accordion__header" onClick={toggleHandler}>
+      <h4 style={styles.accordionTitle}>{props.title}</h4>
    
-      </div>
-      <p className="accordion__content">{props.text}</p>
     </div>
+    <p className="accordion__content">{props.text}</p>
+  </animated.div>
   )
 }
 
