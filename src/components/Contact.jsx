79f0d8a48f4b37ea817911547f3556777d61fc56
ophir0 <147547243+ophir0@users.noreply.github.com>
2024-03-21 15:48:36 +0000

    Implements contact form and sends emails

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,126 +1,76 @@
 import React from 'react'
-import { useState, useRef, useEffect, createRef } from "react";
-import ReCAPTCHA from "react-google-recaptcha";
-import emailjs from '@emailjs/browser';
+import { useState, } from "react";
+import { sendCustomEmail } from "./email";
 
 export const ContactForm = () => {
-    const [status, setStatus] = useState(null);
-    const [formValue, setFormValue] = useState({
-      user_name: "",
-      message: "",
-      user_email: "",
+  
+  const [details, setDetails] = useState({
+    from_name: "",
+    email_address: "",
+    message: "",
+  });
+
+  const handleDetailsChange = (event) => {
+    const { name, value } = event.target;
+
+    setDetails((prevDetails) => {
+      return {
+        ...prevDetails,
+        [name]: value,
+      };
     });
-  
-    const refCaptcha = createRef();
-    const form = useRef();
-  
-    useEffect(() => {
-      let timeout;
-  
-      if (status === true || false) {
-        // Show the info message for 9 seconds
-        timeout = setTimeout(() => {
-          setAlertPrompt(null);
-        }, 10000);
-      }
-  
-      return () => {
-        if (timeout) {
-          clearTimeout(timeout);
-          setStatus(null);
-        }
-      };
-    }, [status]);
-  
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      const token = refCaptcha.current.getValue();
-      setStatus(true);
-  
-      const params = {
-        ...formValue,
-        "g-recaptcha-response": token,
-      };
-  
-      switch (true) {
-        case formValue.user_name === "":
-          setAlertMessage("Name cannot be blank");
-          setStatus(false);
-          break;
-  
-        case formValue.user_email === "":
-          setAlertMessage("Email address cannot be blank");
-          setStatus(false);
-          break;
-  
-        case token === undefined:
-          setAlertMessage("reCAPTCHA error");
-          setStatus(false);
-          break;
-  
-        default:
-          emailjs
-            .sendForm(
-              secret_key.mail_service_id,
-              secret_key.mail_template_id,
-              form.current,
-              secret_key.mail_public_key
-            )
-            .then(
-              (response) => {
-                if (response.status === 200) {
-                  setAlertMessage("Sent successfully");
-                  setStatus(false);
-                  setFormValue({});
-                }
-                // console.log("SUCCESS!", response.status, response.text);
-              },
-              (err) => {
-                setAlertMessage(`Failed: ${err.text}`);
-                setStatus(false);
-                setFormValue({});
-                console.log("FAILED...", err.text);
-              }
-            );
-      }
-    };
-  
-    return (
-      <form ref={form} onSubmit={handleSubmit}>
+  };
+
+
+  const handleSendEmail = () => {
+    sendCustomEmail(details);
+    
+  };
+
+  return (
+    <div className="contactform">
+    <div className="main">
+    <div>
+        <span>Votre prenom:</span>
         <input
+          name="from_name"
+          value={details.from_name}
+          onChange={handleDetailsChange}
           type="text"
-          name="user_name"
-          placeholder="Name"
-          value={formValue.user_name}
-          onChange={(e) =>
-            setFormValue({ ...formValue, user_name: e.target.value })
-          }
+          placeholder="Besoin"
         />
+      </div>
+      <div>
+        <span>Votre email:</span>
         <input
-          type="email"
-          name="user_email"
-          placeholder="Email"
-          value={formValue.user_email}
-          onChange={(e) =>
-            setFormValue({ ...formValue, user_email: e.target.value })
-          }
+          name="email_address"
+          value={details.email_address}
+          onChange={handleDetailsChange}
+          type="text"
+          placeholder="Besoin"
         />
+      </div>
+      <div>
+        <span>Votre message:</span>
         <textarea
           name="message"
-          placeholder="Message"
-          value={formValue.message}
-          rols="5"
-          onChange={(e) =>
-            setFormValue({ ...formValue, message: e.target.value })
-          }
-        ></textarea>
-        <ReCAPTCHA
-          sitekey={secret_key.recaptcha_site_key}
-          ref={refCaptcha}
-          onChange={() => setStatus(null)}
+          value={details.message}
+          onChange={handleDetailsChange}
+          type="text"
+          placeholder="Besoin"
         />
-        <button type="submit">Send</button>
-      </form>
-    );
-  };
+      </div>
+      <button
+        id='submitButton'
+        disabled={ !details.from_name || !details.message || !details.email_address }
+        onClick={handleSendEmail}
+      >
+
+        <span>Send Email</span>
+        
+      </button>
+    </div>
+    </div>
+  );
+};
 export default ContactForm
